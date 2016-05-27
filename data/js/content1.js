5a17c8038ef44888c0db1c38bf1d1a616f3501f9
FrancescoSTL <francescosapolizzi@gmail.com>
2016-05-27 15:30:56 -0700

    Additional styling changes.

--- data/js/content1.js
+++ data/js/content1.js
@@ -6,7 +6,7 @@
     // listen for a click on the search button
     searchButton.addEventListener('click', function() {
 		token1.style.backgroundImage = "url('/data/media/utility/content1-token.svg')";
-
+		
         // notify addon that we've clicked the button
         addon.port.emit('searchClick');
     });
