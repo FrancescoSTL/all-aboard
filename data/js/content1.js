1c35c3d1d22b3a7090e2611ffac8b2a355e50f9c
FrancescoSTL <francescosapolizzi@gmail.com>
2016-05-30 23:38:45 -0700

    Add initial flow for value sidebar.

--- data/js/content1.js
+++ data/js/content1.js
@@ -1,13 +1,56 @@
 (function() {
     // retrieve button from the page
-    var searchButton = document.querySelector('#search_button');
+    var searchButton = document.querySelector('#answer_button');
+    var mozillaRadio = document.getElementById('mozilla');
+    var mozillaOption = document.getElementById('mozilla-Option');
+    var kidRadio = document.getElementById('kid');
+    var singersRadio = document.getElementById('singers');
     var token1 = document.querySelector('#token1');
+    var answered = false;
     
     // listen for a click on the search button
     searchButton.addEventListener('click', function() {
-		token1.style.backgroundImage = "url('/data/media/utility/content1-token.svg')";
-		
-        // notify addon that we've clicked the button
-        addon.port.emit('searchClick');
+        if(!answered)
+        {
+    		token1.style.backgroundImage = "url('/data/media/values/content1-token.svg')";
+    		
+            if(kidRadio.checked)
+            {
+                mozillaRadio.checked = true;
+                kidRadio.checked = false;
+
+                mozillaRadio.disabled = true;
+                kidRadio.disabled = true;
+                singersRadio.disabled = true;
+                answered = true;
+
+                mozillaOption.style.backgroundColor = "#87D37C";
+                mozillaOption.style.color = "#F2F1EF";
+            }
+            else 
+                if(singersRadio.checked)
+                {
+                    mozillaRadio.checked = true;
+                    singersRadio.checked = false;
+
+                    mozillaRadio.disabled = true;
+                    kidRadio.disabled = true;
+                    singersRadio.disabled = true;
+                    answered = true;
+
+                    mozillaOption.style.backgroundColor = "#87D37C";
+                    mozillaOption.style.color = "#F2F1EF";
+                }
+                else
+                {
+                    mozillaRadio.disabled = true;
+                    kidRadio.disabled = true;
+                    singersRadio.disabled = true;
+                    answered = true;
+
+                    mozillaOption.style.backgroundColor = "#87D37C";
+                    mozillaOption.style.color = "#F2F1EF";
+                }
+        }
     });
 })();
